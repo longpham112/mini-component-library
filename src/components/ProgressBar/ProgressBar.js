860b2f4a46360c1ddeddf769d6e0ab4a8d42a283
chilikilong112 <chiliki.long@gmail.com>
2023-04-25 23:08:18 +1000

    start with a customised progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuemax="100"
+      aria-valuemin="0"
+      aria-valuenow={value}
+    >
+      <Bar />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  background-color: ${COLORS.transparentGray15};
+`;
+
+const Bar = styled.div`
+  height: 8px;
+  width: 100%;
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
