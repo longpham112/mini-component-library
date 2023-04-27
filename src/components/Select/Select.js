c051eb40d5951b16ece160e875219e842f8a0418
chilikilong112 <chiliki.long@gmail.com>
2023-04-27 17:16:01 +1000

    create wrapper and decorational div

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <DecorationBit>Content</DecorationBit>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled.div``;
+
+const NativeSelect = styled.select``;
+
+const DecorationBit = styled.div``;
 export default Select;
